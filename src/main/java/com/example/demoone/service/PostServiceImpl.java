59e3a52566c4c20f5246a68b25423fe675a2cf11
Zoia Tsitsilashvili <zoika.leli@gmail.com>
2022-12-19 22:34:03 +0400

    add Post_ and User_ class commit

--- src/main/java/com/example/demoone/service/PostServiceImpl.java
+++ src/main/java/com/example/demoone/service/PostServiceImpl.java
@@ -2,7 +2,9 @@ package com.example.demoone.service;
 
 import com.example.demoone.dtu.PostSearchParams;
 import com.example.demoone.entity.Post;
+import com.example.demoone.entity.Post_;
 import com.example.demoone.entity.User;
+import com.example.demoone.entity.User_;
 import com.example.demoone.exception.NotFoundException;
 import com.example.demoone.repository.PostRepository;
 import org.apache.commons.lang3.StringUtils;
@@ -31,26 +33,26 @@ public class PostServiceImpl implements PostService {
         return postRepository.findAll((root, query, cb) -> {
             Predicate predicate = cb.conjunction();
             if(params.getId()!= null) {
-                predicate = cb.and(predicate, cb.equal(root.get("id"), params.getId()));
+                predicate = cb.and(predicate, cb.equal(root.get(Post_.ID), params.getId()));
             }
             // if(params.getTitle() != null && !params.getTitle().isEmpty())
             if(StringUtils.isNotEmpty(params.getTitle())) {
-                predicate = cb.and(predicate, cb.like(root.get("title"), '%' + params.getTitle() + '%'));
+                predicate = cb.and(predicate, cb.like(root.get(Post_.TITLE), '%' + params.getTitle() + '%'));
             }
             if(StringUtils.isNotEmpty(params.getBody())) {
-                predicate = cb.and(predicate, cb.like(root.get("body"), '%' + params.getBody() + '%'));
+                predicate = cb.and(predicate, cb.like(root.get(Post_.BODY), '%' + params.getBody() + '%'));
             }
             if(params.getCreateDateFrom() != null){
                var createDateFrom = params.getCreateDateFrom().atStartOfDay();
-                predicate = cb.and(predicate, cb.greaterThanOrEqualTo(root.get("createDate"), createDateFrom));
+                predicate = cb.and(predicate, cb.greaterThanOrEqualTo(root.get(Post_.CREATE_DATE), createDateFrom));
             }
             if(params.getCreateDateTo() != null){
                 var creatDateTo = params.getCreateDateTo().atTime(23, 59, 59);
-                predicate = cb.and(predicate, cb.lessThanOrEqualTo(root.get("createDate"), creatDateTo));
+                predicate = cb.and(predicate, cb.lessThanOrEqualTo(root.get(Post_.CREATE_DATE), creatDateTo));
             }
             if(StringUtils.isNotEmpty(params.getUsername())){
-                Join<Post, User> user = root.join("user", JoinType.LEFT);
-                predicate = cb.and(predicate, cb.like(user.get("username"), params.getUsername() + '%'));
+                Join<Post, User> user = root.join(Post_.USER, JoinType.LEFT);
+                predicate = cb.and(predicate, cb.like(user.get(User_.USERNAME), params.getUsername() + '%'));
             }
             return predicate;
         }, pageable);
